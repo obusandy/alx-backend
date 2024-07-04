73121dbc58d74d9b912fbdd987cd2dcbdbb95e82
obusandy <obungasandrah@gmail.com>
2024-07-04 12:08:18 +0000

    Parametrize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python3
 """
-Setup a basic Flask app with Babel
+below is a basic Flask app with Babel
 """
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
+
 class Config(object):
     """
-    Config class
-    that has a LANGUAGES class attribute
-    Holds configurations for languages and time zone.
+    handles configs for supported languages and time zone.
     """
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC' # time zone.
+    BABEL_DEFAULT_LOCALE = 'en' # language
+    BABEL_DEFAULT_TIMEZONE = 'UTC' # time
 
 
-# Initialize the Flask app
-
+# Flask app
 app = Flask(__name__)
-# Load configurations
+# configuration
 app.config.from_object(Config)
 
 babel = Babel(app)
@@ -28,16 +26,18 @@ babel = Babel(app)
 @babel.localeselector
 def get_locale() -> str:
     """
-    Uses the 'Accept-Language' header from the request
-    to select the most appropriate language
+    Returns the language specified in query parameter 'locale' if valid
+    best match from accepted languages.
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
-
+"""
+main point of entry
+"""
 @app.route('/', strict_slashes=False)
 def index() -> str:
     """
-    Render the index.html template
+    renders the indexhtml template
     """
     return render_template('3-index.html')
 
